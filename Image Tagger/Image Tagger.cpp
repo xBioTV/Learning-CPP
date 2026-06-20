6756581003815a95b7059e1b09300b7950304931
xBioTV <statixhdpro@gmail.com>
2026-06-20 14:27:06 -0400

    Created "Image Tagger" project, renamed Solution to "CPP Projects"

--- Image Tagger/Image Tagger.cpp
+++ Image Tagger/Image Tagger.cpp
@@ -1,20 +1,34 @@
-// Image Tagger.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <filesystem>
+#include <vector>
+
+struct imageFiles
+{
+    imageFiles();
+    imageFiles(std::string path[]);
+    imageFiles(std::string path[], std::string extension[]);
+    imageFiles(std::string path[], std::string name[], std::string idTag[]);
+
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //Todo
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+imageFiles LoadImagesFromFolder(std::string path[], std::string extension[])
+{
+    /**Psuedo Code**
+    SET path to user input
+    IF path isn't empty
+        LOOP through files until all of them are checked
+            STORE image files in std::string path[FILE_COUNT]
+            PRINT to extension[FILE_COUNT] to console
+            PRINT to path[FILE_COUNT] count to console
+            RETURN imageFiles();
+    ELSE
+        PRINT no matching files in path to console
+        RETURN;
+    ***************/
+}
